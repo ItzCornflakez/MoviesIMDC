15f79ca38af94556b3229a7dc2d2471878188085
Emil Nyberg <emilnyberg1998@gmail.com>
2023-04-20 16:17:36 +0200

    Works now

--- app/src/main/java/com/ltu/m7019e/v23/themoviedb/utils/Constants.kt
+++ app/src/main/java/com/ltu/m7019e/v23/themoviedb/utils/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val IMDB_BASE_URL = "https://www.imdb.com/title/"
     const val POSTER_IMAGE_WIDTH = "w92"
     const val BACKDROP_IMAGE_WIDTH = "w1280"
-    const val MOVIE_LIST_BASE_URL = "https://developers.themoviedb.org/3/movie/"
+    const val MOVIE_LIST_BASE_URL = "https://api.themoviedb.org/3/movie/"
     const val API_KEY = "f6e17593532633ba72a5da0ad3bdf3e1"
 
 }
